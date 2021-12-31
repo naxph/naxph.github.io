ba0e87bdaef47f92b6c971ec09bf3eacfe7b7329
Nax <thatonecoder420@gmail.com>
2022-01-01 03:21:52 +0800

    Added hover events to social icons and implemented Typeform

--- js/scripts.js
+++ js/scripts.js
@@ -41,6 +41,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
   })
 })
 
+//Typed.js
 var typed = new Typed("#typed", {
   strings: [
     "Web Development^1000",
@@ -56,3 +57,166 @@ var typed = new Typed("#typed", {
   typeSpeed: 50,
   loop: true,
 })
+
+var typed = new Typed("#about-typed", {
+  strings: [
+    "Anime",
+    "Games",
+    "Global Politics",
+    "Formula 1",
+    "Machine Learning",
+    "AI",
+    "Cybersecurity",
+    "Game Development",
+    "Technology",
+  ],
+  backSpeed: 40,
+  typeSpeed: 40,
+  loop: true,
+  smartBackspace: true,
+})
+
+//Social Hover Event Triggers
+$("#social-twitter").hover(
+  function () {
+    $(".masthead").css("background", "#1DA1F2")
+    $(".hero-emphasis").css("color", "#4A00E0")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-instagram").hover(
+  function () {
+    $(".masthead").css("background", "#C13584")
+    $(".hero-emphasis").css("color", "yellow")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-github").hover(
+  function () {
+    $(".masthead").css("background", "#333")
+    $(".hero-emphasis").css("color", "#FBD786")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-youtube").hover(
+  function () {
+    $(".masthead").css("background", "#FF0000")
+    $(".hero-emphasis").css("color", "#F5F5DC")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-whatsapp").hover(
+  function () {
+    $(".masthead").css("background", "#25D366")
+    $(".hero-emphasis").css("color", "#FC8EAC")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-unsplash").hover(
+  function () {
+    $(".masthead").css("background", "#000000")
+    $(".hero-emphasis").css("color", "#FFFFFF")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-blog").hover(
+  function () {
+    $(".masthead").css("background", "#f5af19")
+    $(".hero-emphasis").css("color", "#1a2a6c")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
+
+$("#social-steam").hover(
+  function () {
+    $(".masthead").css("background", "#1b2838")
+    $(".hero-emphasis").css("color", "#FFF200")
+  },
+  function () {
+    $(".masthead").css(
+      "background",
+      "linear-gradient(to bottom,rgba(0, 0, 0, 0.3) 0%,rgba(0, 0, 0, 0.7) 75%,#000 100%),url('../assets/img/musicvideo.gif'"
+    )
+    $(".masthead").css("background-position", "center")
+    $(".masthead").css("background-repeat", "no-repeat")
+    $(".masthead").css("background-attachment", "scroll")
+    $(".masthead").css("background-size", "cover")
+    $(".hero-emphasis").css("color", "coral")
+  }
+)
